1a880df2855dff11ee06420f13cc4d8fe0b8de4b
rit-yi <jyunyi501@richmond.com.tw>
2026-05-22 18:06:58 +0800

    update pokopia

--- pokopia/admin.py
+++ pokopia/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Favorite, Habitat, Item, Pokemon, PokemonType, Specialty
+
+
+@admin.register(PokemonType, Habitat, Specialty, Favorite)
+class NamedLookupAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = (
+        "pokopia_dex_number",
+        "name",
+        "classification",
+        "preferred_environment",
+        "flavor",
+        "is_unique_npc",
+        "is_event",
+    )
+    list_filter = (
+        "preferred_environment",
+        "flavor",
+        "is_unique_npc",
+        "is_event",
+        "types",
+        "habitats",
+        "specialties",
+        "favorites",
+    )
+    search_fields = (
+        "name",
+        "classification",
+        "description",
+        "types__name",
+        "habitats__name",
+        "specialties__name",
+        "favorites__name",
+    )
+    filter_horizontal = ("types", "habitats", "specialties", "favorites")
+    ordering = ("pokopia_dex_number", "name")
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "tag")
+    list_filter = ("category", "tag", "favorites")
+    search_fields = ("name", "description", "favorites__name")
+    filter_horizontal = ("favorites",)
+    ordering = ("category", "name")
